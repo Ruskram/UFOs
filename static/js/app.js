0da6a14f8f9dc97b15b6023f5b99b143f7bf550e
Rus Kram <ruskram87@gmail.com>
2022-02-20 20:12:17 -0500

    clean up

--- static/js/app.js
+++ static/js/app.js
@@ -37,10 +37,9 @@ function updateFilters() {
     
     // 4b. Save the value that was changed as a variable.
     let changedValue = changedElement.property("value");
-    console.log(changedValue);
+
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
-    console.log(filterId);
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (changedValue) {
